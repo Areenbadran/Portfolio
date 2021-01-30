add9cc90b277604b012f93b0411fbc6bec7e951d
AreenBadran <areenbadran9@gmail.com>
2021-01-30 17:06:25 +0200

    Portfolio Final

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react';
-import './App.css';
-import Sidebar from './components/sidebar';
-import Home from './components/home';
-import About from './components/about';
-import Timeline from './components/timeline';
-import Projects from './components/projects';
-import Skills from './components/skills';
+import React, { Component } from "react";
+import "./App.css";
+import Sidebar from "./components/sidebar";
+import Home from "./components/home";
+import About from "./components/about";
+import Timeline from "./components/timeline";
+import Projects from "./components/projects";
+import Skills from "./components/skills";
 
 class App extends Component {
   render() {
     return (
       <div id="colorlib-page">
         <div id="container-wrap">
-         	<Sidebar></Sidebar>
-				<div id="colorlib-main">
-					<Home></Home>
-					<About></About>
-					<Timeline></Timeline>
-          <Projects></Projects>
-          <Skills></Skills>
-          	</div>
-      	</div>
+          <Sidebar></Sidebar>
+          <div id="colorlib-main">
+            <Home></Home>
+            <About></About>
+            <Timeline></Timeline>
+            <Projects></Projects>
+            <Skills></Skills>
+          </div>
+        </div>
       </div>
     );
   }
