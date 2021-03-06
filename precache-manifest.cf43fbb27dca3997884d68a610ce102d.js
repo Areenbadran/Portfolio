3a0a5c7725447b9c6c1961cc0801f3d7b51d9461
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-03-06 13:30:16 +0000

    Deploy Areenbadran/Portfolio to github.com/Areenbadran/Portfolio.git:gh-pages

---
+++ precache-manifest.cf43fbb27dca3997884d68a610ce102d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9d02da80d99a49ca2a7",
+    "url": "/Portfolio/static/css/main.52140cc0.chunk.css"
+  },
+  {
+    "revision": "b9d02da80d99a49ca2a7",
+    "url": "/Portfolio/static/js/main.b9d02da8.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.6b64d540.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/Portfolio/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "76595c0f518b53451990807e4d5f93df",
+    "url": "/Portfolio/index.html"
+  }
+];
